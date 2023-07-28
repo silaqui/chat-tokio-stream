38c4cc4bfe7d40b62bf2a3f330cbaa4fb241932f
Miki <aivi.pol@gmail.com>
2023-07-28 22:30:51 +0200

    Implement simple chat service with tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpListener;
+use tokio::sync::broadcast;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (tx, _rx) = broadcast::channel(10);
+
+    loop {
+        let (mut socket, address) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+
+                        tx.send((line.clone(),address)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if address != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
